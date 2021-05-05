7293b102b6e30d404170041913509cfbf0e65217
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-05 18:31:18 +0100

    minor tweaks

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from .secrete_settings import my_key
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = '/staticfiles/sent_emails/'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'staticfiles/sent_emails')
 
 django_heroku.settings(locals())
