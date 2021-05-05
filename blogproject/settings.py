b2098c9d5794f0a3412dad8095c4344ac1ad24c7
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-05 13:38:21 +0100

    minor tweaks

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+EMAIL_FILE_PATH = str(BASE_DIR.joinpath('staticfiles').joinpath('sent_emails'))
 
 django_heroku.settings(locals())
