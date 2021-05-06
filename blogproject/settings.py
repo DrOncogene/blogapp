94b97f145676088d9e17837381581d518be72ef1
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-06 04:05:50 +0100

    minor tweaks

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from .secrete_settings import my_key
 import django_heroku
 import dj_database_url
 import os
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_key
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
