aa564b6f95574fe30f73cd62235582c2a8550d58
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-06 04:54:32 +0100

    fixed user registration issues

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -10,7 +10,7 @@ class NewPostForm(forms.ModelForm):
     model = Post
     fields = ['title', 'body']
 
-class SignUpForm(auth_forms.UserCreationForm):
+class SignUpForm(auth_forms.UserCreationForm, forms.ModelForm):
   class Meta:
     model: 'auth.User'
     fields = ['username', 'email', 'first_name', 'last_name', 'password']
