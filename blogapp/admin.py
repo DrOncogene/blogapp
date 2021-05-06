ed0e115011bd5c4b46331ab00fe4e8eb0d9edbc2
Rasheed Nasir <mypythtesting@gmail.com>
2021-05-06 10:01:05 +0100

    changes to the signup form

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, Comment
+from .models import Post, Comment, Author
 # Register your models here.
 
+admin.site.register(Author)
 admin.site.register(Post)
 admin.site.register(Comment)
